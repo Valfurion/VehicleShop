6567a658b4a8128dd40bcfcab5052d9190274222
Valfurion <Alexeimv246@mail.ru>
2020-01-07 11:53:23 +0400

    Прописал зависимости JDBC и PostgreSQL в POM.xml и прописал подключение к БД в PostgreSQL

---
+++ src/main/java/org/VehicleShop/VehicleShopApplication.java
@@ -0,0 +1,13 @@
+package org.VehicleShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/VehicleShop/VehicleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.VehicleShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
