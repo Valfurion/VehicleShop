fb90c480ed29265a6f069c67494bff3f9cf76405
Valfurion <Alexeimv246@mail.ru>
2020-01-14 12:34:03 +0400

    написал PutMapping для EngineType

--- src/main/java/org/VehicleShop/controller/EngineTypeController.java
+++ src/main/java/org/VehicleShop/controller/EngineTypeController.java
@@ -1,5 +1,6 @@
 package org.VehicleShop.controller;
 
+import org.VehicleShop.entity.Engine;
 import org.VehicleShop.entity.EngineType;
 import org.VehicleShop.repository.EngineTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,11 @@ public class EngineTypeController {
     @PostMapping
     public EngineType createEngineType(@RequestBody EngineType engineType) {
         return engineTypeRepository.createEngineType(engineType);
+    }
+
+    @PutMapping("/change/{id}")
+    public EngineType changeEngineType(@PathVariable Long id,@RequestBody EngineType engineType){
+        engineType.setEngineTypeId(id);
+        return engineTypeRepository.changeEngineType(engineType);
     }
 }
